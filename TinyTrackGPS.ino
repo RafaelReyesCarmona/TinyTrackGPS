a796498786a93ad8303fa7d7aca9683eca41a743
RafaelReyesCarmona <78477791+RafaelReyesCarmona@users.noreply.github.com>
2021-02-15 14:01:38 +0100

    Update TinyTrackGPS.ino

--- TinyTrackGPS.ino
+++ TinyTrackGPS.ino
@@ -1,4 +1,25 @@
+/*
+TinyTrackGPS.ino
 
+Copyright © 2014-2021 Francisco Rafael Reyes Carmona. 
+rafael.reyes.carmona@gmail.com 
+
+  This file is part of TinyTrackGPS. 
+
+  TinyTrackGPS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  TinyTrackGPS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with TinyTrackGPS.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 
 /*
      Programa de localizacion por gps que graba las posiciones en
